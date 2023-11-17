b9cc810f516c688911e9db72b8a97363be987a22
AdrianRevi <adrianreviriego@gmail.com>
2023-11-17 18:32:05 +0100

    COMMIT cambio_card

--- code/añadirObras.js
+++ code/añadirObras.js
@@ -6,18 +6,19 @@ xhttp.onreadystatechange = function() {
         var elementosHtml = '';
 
         data.forEach(function(elemento) {
-            elementosHtml += '<div class="card mb-3">';
-            elementosHtml += '<img src="assets/images/' + elemento.Imagen + '" alt="Imagen del Elemento" class="card-img-top">';
+            elementosHtml += '<div class="col-md-4 mb-4">'; // Div con clase col-md-4 para ocupar 1/3 del ancho en pantallas medianas y grandes
+            elementosHtml += '<div class="card">';
+            elementosHtml += '<img src="assets/images/' + elemento.Imagen + '" alt="Imagen del Elemento" class="card-img-top img-thumbnail">';
             elementosHtml += '<div class="card-body">';
             elementosHtml += '<h5 class="card-title">' + elemento.Nombre + '</h5>';
             elementosHtml += '<p class="card-text">Autor: ' + elemento.Autor + '</p>';
             elementosHtml += '<p class="card-text">Precio: ' + elemento.Precio + '</p>';
             elementosHtml += '</div>';
             elementosHtml += '</div>';
+            elementosHtml += '</div>'; // Cerrar div con clase col-md-4
         });
         
-
-        document.getElementById("elementos").innerHTML = elementosHtml;
+        document.getElementById("elementos").innerHTML = '<div class="row">' + elementosHtml + '</div>';        
     }
 };
 xhttp.open("GET", "datos.php", true);
